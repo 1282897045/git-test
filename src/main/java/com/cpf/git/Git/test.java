ec4b6b5bbf0ac57c5dfc8ad8e9077c57ea67e915
chaipengfei <1282897045qq@.com>
2022-01-04 14:19:00 +0800

    second commit

--- src/main/java/com/cpf/git/Git/test.java
+++ src/main/java/com/cpf/git/Git/test.java
@@ -3,6 +3,8 @@ package com.cpf.git.Git;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        System.out.println("hello git3!");
+        System.out.println("hello git2 !");
 
     }
 }
